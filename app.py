9fc81bafe92eb0393ac64a2e7c217ed1c77d2a57
Ritvick Maini <101634547+Ritvickmaini@users.noreply.github.com>
2026-01-15 16:03:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,6 @@ def generate_email_html(
                   Regards<br/>
                   Customer Success Team<br/>
                   Best Business Award London</br>
-                  (+44) 2034517166<br/>
                   3–4 March 2026 | London Olympia<br/>
                   <br/>
                   On Behalf of<br/>
